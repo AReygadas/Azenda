78611f3b0126670b51cca274edfd74c4868d6cee
HYussef <ugo_mellado@hotmail.com>
2024-08-03 11:18:23 -0600

    Manejo de propiedades BackEnd v2(Models,Controllers,Services)

--- WebAPI/Controllers/Property.Controller.js
+++ WebAPI/Controllers/Property.Controller.js
@@ -13,6 +13,60 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+router.get('/CB', async (req, res, next) => {
+    try {
+        const properties = await service.getAllPropertiesCB();
+        res.json(properties);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/CU', async (req, res, next) => {
+    try {
+        const properties = await service.getAllPropertiesCU();
+        res.json(properties);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/H', async (req, res, next) => {
+    try {
+        const properties = await service.getAllPropertiesH();
+        res.json(properties);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/IB', async (req, res, next) => {
+    try {
+        const properties = await service.getAllPropertiesIB();
+        res.json(properties);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/O', async (req, res, next) => {
+    try {
+        const properties = await service.getAllPropertiesO();
+        res.json(properties);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/W', async (req, res, next) => {
+    try {
+        const properties = await service.getAllPropertiesW();
+        res.json(properties);
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.get('/:id',
     async (req, res, next) => {
         try {
@@ -25,10 +79,142 @@ router.get('/:id',
     }
 );
 
+router.get('/CB/:id',
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const property = await service.findOneCB(id);
+            res.json(property);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get('/CU/:id',
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const property = await service.findOneCU(id);
+            res.json(property);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get('/H/:id',
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const property = await service.findOneH(id);
+            res.json(property);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get('/IB/:id',
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const property = await service.findOneIB(id);
+            res.json(property);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get('/O/:id',
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const property = await service.findOneO(id);
+            res.json(property);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get('/W/:id',
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const property = await service.findOneW(id);
+            res.json(property);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
 router.post('/add',async (req, res, next) => {
     try {
         const body = req.body;
         const newProperty = await service.create(body);
+        res.status(201).json(newProperty);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/addCB',async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newProperty = await service.createCB(body);
+        res.status(201).json(newProperty);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/addCU',async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newProperty = await service.createCU(body);
+        res.status(201).json(newProperty);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/addH',async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newProperty = await service.createH(body);
+        res.status(201).json(newProperty);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/addIB',async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newProperty = await service.createIB(body);
+        res.status(201).json(newProperty);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/addO',async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newProperty = await service.createO(body);
+        res.status(201).json(newProperty);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/addW',async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newProperty = await service.createW(body);
         res.status(201).json(newProperty);
     } catch (error) {
         next(error);
--- WebAPI/Services/PropertyService.js
+++ WebAPI/Services/PropertyService.js
@@ -6,16 +6,109 @@ class PropertyService{
     constructor(){
 
     }
+
     async create(data) {
         const newProperty = await models.CommercialProperty.create(data);
         return newProperty;
     }
+    async createCB(data) {
+        const newProperty = await models.CommercialBuilding.create(data);
+        return newProperty;
+    }
+    async createCU(data) {
+        const newProperty = await models.CommercialUnit.create(data);
+        return newProperty;
+    }
+    async createH(data) {
+        const newProperty = await models.Hotel.create(data);
+        return newProperty;
+    }
+    async createIB(data) {
+        const newProperty = await models.IndustrialBuilding.create(data);
+        return newProperty;
+    }
+    async createO(data) {
+        const newProperty = await models.Office.create(data);
+        return newProperty;
+    }
+    async createW(data) {
+        const newProperty = await models.Warehouse.create(data);
+        return newProperty;
+    }
+
     async getAllProperties(){
         const rta = await models.CommercialProperty.findAll();
         return rta;
     }
+    async getAllPropertiesCB(){
+        const rta = await models.CommercialBuilding.findAll();
+        return rta;
+    }
+    async getAllPropertiesCU(){
+        const rta = await models.CommercialUnit.findAll();
+        return rta;
+    }
+    async getAllPropertiesH(){
+        const rta = await models.Hotel.findAll();
+        return rta;
+    }
+    async getAllPropertiesIB(){
+        const rta = await models.IndustrialBuilding.findAll();
+        return rta;
+    }
+    async getAllPropertiesO(){
+        const rta = await models.Office.findAll();
+        return rta;
+    }
+    async getAllPropertiesW(){
+        const rta = await models.Warehouse.findAll();
+        return rta;
+    }
+
     async findOne(id) {
         const property = await models.CommercialProperty.findByPk(id);
+        if (!property) {
+            throw boom.notFound('user not found');
+        }
+        return property;
+    }
+    async findOneCB(id) {
+        const property = await models.CommercialBuilding.findByPk(id);
+        if (!property) {
+            throw boom.notFound('user not found');
+        }
+        return property;
+    }
+    async findOneCU(id) {
+        const property = await models.CommercialUnit.findByPk(id);
+        if (!property) {
+            throw boom.notFound('user not found');
+        }
+        return property;
+    }
+    async findOneH(id) {
+        const property = await models.Hotel.findByPk(id);
+        if (!property) {
+            throw boom.notFound('user not found');
+        }
+        return property;
+    }
+    async findOneIB(id) {
+        const property = await models.IndustrialBuilding.findByPk(id);
+        if (!property) {
+            throw boom.notFound('user not found');
+        }
+        return property;
+    }
+    async findOneO(id) {
+        const property = await models.Office.findByPk(id);
+        if (!property) {
+            throw boom.notFound('user not found');
+        }
+        return property;
+    }
+    async findOneW(id) {
+        const property = await models.Warehouse.findByPk(id);
         if (!property) {
             throw boom.notFound('user not found');
         }
